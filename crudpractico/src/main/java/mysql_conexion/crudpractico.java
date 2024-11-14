f4829ca1fa5901e04b70745572a705d9c482bae5
Christopher Lucas Leyva <168372093+ChristopherLeyva@users.noreply.github.com>
2024-11-14 08:58:39 -0500

    crudpractico

---
+++ crudpractico/src/main/java/mysql_conexion/crudpractico.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mysql_conexion;
+
+import conexion.conexionsql;
+
+/**
+ *
+ * @author Usuario
+ */
+public class crudpractico {
+
+    public static void main(String[] args) {
+        conexionsql padre = new conexionsql();
+        padre.getConexion();
+    }
+}
